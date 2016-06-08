23707b62a5f92894178d7f35359f38544194aec5
Camilo <jcperez2@wic.edu>
2016-06-08 08:44:22 -0500

    added z orientation

--- ex_vol.f90
+++ ex_vol.f90
@@ -1,6 +1,15 @@
 module distances
 implicit none
+
+type bound_box
+real(8), dimension(3):: X
+real(8), dimension(3):: W
+end type bound_box
+
 contains
+
+
+
 subroutine dist_segs(ra,&
                      ra_end,&
                      rb,&
@@ -91,9 +100,170 @@ real(8)              :: Gab_norm
 Gab=ra-rb
 Gab_norm=sqrt(dot_product (Gab, Gab))
 
-end subroutine dist_pt_pt
+                      end subroutine dist_pt_pt
+
+                      
+                      
+function clamp( a, b, c)
+real(8)              ::a, b, c, clamp
+
+if ( a < b ) then
+clamp = b
+return
+end if
+
+if ( a > c ) then
+clamp = c
+return
+end if
+
+clamp = a
+end function clamp
+ 
+subroutine dist_segments(p1,&
+                     q1,&
+                     p2,&
+                     q2,&
+                     s,&
+                     t,&
+                     Gab,&
+                     Gab_norm)
+
+
+real(8), dimension(3):: p1, q1, p2, q2, d1, d2, Gab, r, c1, c2
+real(8)              ::Gab_norm, epsilon, s, t
+real(8)              ::a, e, f, c, b, denom
+logical              :: coll_course
+
+epsilon  =1D-40
+
+d1=q1-p1
+d2=q2-p2
+
+r = p1 - p2
+
+a = dot_product(d1,d1)
+e = dot_product(d2,d2)
+f = dot_product(d2,r)
+
+if ((a<=epsilon) .and. (e<=epsilon)  ) then
+    s = 0.0D0
+    t = 0.0D0
+    
+    c1 = p1
+    c2 = p2
+    
+    Gab_norm = sqrt(dot_product(c1 - c2, c1 - c2))
+    Gab =  (c1 - c2)
+    return
+end if
+    
+if ( a<= epsilon ) then
+    s = 0D0
+    t = f/e
+    t = clamp(t, 0.0D0, 1.0D0)    
+else
+    c = dot_product(d1,r)
+    
+    if(e <= epsilon)then
+        t= 0.0D0
+        s = clamp(-c /a, 0.0D0, 1.0D0)
+    else
+        b = dot_product(d1, d2)
+        denom = a*e-b*b
+        
+        if(denom /=	0.0D0 ) then
+            s = clamp((b*f -c*e) / denom  , 0.0D0, 1.0D0) 
+        else
+         s =0.0D0
+        end if
+        
+        t =(b*s +f) / e
+        
+        if (t < 0D0) then
+            t = 0.0D0
+            s =clamp(-c / a, 0.0D0 , 1.0D0)
+        else if ( t> 1.0) then
+            
+            t= 1.0D0
+            s =clamp((b-c)/a, 0.0D0, 1.0D0)
+            
+       end if
+        
+        
+   end if
+
+
+    
+end if
+
+    c1 = p1 + d1*s
+    c2 = p2 + d2*t
+    !print *, "c1 " , c1
+    !print *, "c2 " , c2
+     !print *, "s " , s
+     !print *, "t " , t 
+     !print *, "p1 " , p1 
+     !print *, "p2 " , p2 
+     !print *, "d1 " , d1 
+     !print *, "d2 " , d2 
+     !print *, " "
+    Gab_norm = sqrt(dot_product(c1 - c2, c1 - c2))
+    Gab =  (c1 - c2)
+
+
+end subroutine dist_segments
 
 !------------------------------------------------------
+function are_boxes_intersect(hinge_pt1, hinge_pt2,segment_pt1, segment_pt2, fiber_diameter)
+
+logical               ::are_boxes_intersect
+real(8), dimension(3) ::hinge_pt1, hinge_pt2,segment_pt1, segment_pt2 
+type(bound_box)       ::a, b
+real(8)               :: fiber_diameter
+are_boxes_intersect=.false.
+a.X=(hinge_pt1+hinge_pt2)/2
+a.W=abs(hinge_pt1-hinge_pt2)+fiber_diameter
+
+
+b.X=(segment_pt1+segment_pt2)/2
+b.W=abs(segment_pt1-segment_pt2)+fiber_diameter
+
+
+are_boxes_intersect= (abs(a.X(1) - b.X(1)) * 2 < (a.W(1) + b.W(1))) .and.&
+                     (abs(a.X(2) - b.X(2)) * 2 < (a.W(2) + b.W(2))) .and.&
+                     (abs(a.X(3) - b.X(3)) * 2 < (a.W(3) + b.W(3)))  
+
+end function are_boxes_intersect
+
+                     
+subroutine collide2   (ra,&
+                     ra_end,&
+                     rb,&
+                     rb_end,&
+                     fiber_diameter,&
+                     fibs_collide)
+                     
+
+real(8), dimension(3):: ra, ra_end, rb, rb_end, Gab
+real(8)              :: s, t, Gab_min, fiber_diameter
+logical              :: fibs_collide 
+
+
+call dist_segments(ra,&
+               ra_end,&
+               rb,&
+               rb_end,&
+                     s,&
+                     t,&
+                     Gab,&
+                     Gab_min)
+
+if (Gab_min .LT. fiber_diameter) fibs_collide = .true.
+
+
+end subroutine collide2
+
 subroutine collide   (ra,&
                      ra_end,&
                      rb,&
--- main.f90
+++ main.f90
@@ -1,232 +1,273 @@
-!LOCATES FIBERS ACCORDING TO A FILE WITH ORIENTATION
+    !LOCATES FIBERS ACCORDING TO A FILE WITH ORIENTATION
 
-!*********************************************
-!*********************************************
+    !*********************************************
+    !*********************************************
 
-program fiber_position
-use distances
-implicit none
-real(8), parameter                   :: pi=3.14159
-real(8), dimension(3)                :: box_size
-real(8), dimension(3)                :: box_location
-real(8), dimension(3)                :: ini_pos
-real(8), dimension(3)                :: vec_unit
-real(8)                              :: fiber_length            
-real(8)                              :: direction
-real(8)                              :: max_theta
-real(8)                              :: phi
-real(8)                              :: theta
-real(8)                              :: total_length
-real(8)                              :: total_length_simulation
-real(8)                              :: volumetric_fraction
-real(8)                              :: fiber_diameter
-real(8)                              :: vol_frac
-real(8)                              :: r
-real(8)                              :: segment_length
-real(8),dimension(:,:), allocatable  :: coords_ini
-real(8),dimension(:,:), allocatable  :: coords_end
-real(8),dimension(3)                 :: coords
-integer                              :: i, j, l, k, nbr_bins, kk, jj, ll
-integer                              :: nbr_fibers
-integer                              :: nbr_segments
-integer                              :: orientation
-!*********************************************
-real(8), dimension(3)                :: ra, ra_end, rb, rb_end, pa, pb, Gab
-real(8)                              :: Sab, Sba, la, lb, Gab_norm
-logical                              :: flag, fibs_collide
-real(8), dimension(:),  allocatable  :: bin_centers
-real(8), dimension(:),  allocatable  :: bin_heights
-real(8), dimension(:,:),allocatable  :: bin_limits
-real(8)                              :: del_bin
-!*********************************************
+    program fiber_position
+    use distances
+    implicit none
+    real(8), parameter                   :: pi=3.14159
+    real(8), dimension(3)                :: box_size
+    real(8), dimension(3)                :: box_location
+    real(8), dimension(3)                :: ini_pos
+    real(8), dimension(3)                :: vec_unit
+    real(8)                              :: fiber_length            
+    real(8)                              :: direction
+    real(8)                              :: max_theta
+    real(8)                              :: phi
+    real(8)                              :: theta
+    real(8)                              :: total_length
+    real(8)                              :: total_length_simulation
+    real(8)                              :: volumetric_fraction
+    real(8)                              :: fiber_diameter
+    real(8)                              :: vol_frac
+    real(8)                              :: r
+    real(8)                              :: segment_length
+    real(8),dimension(:,:), allocatable  :: coords_ini
+    real(8),dimension(:,:), allocatable  :: coords_end
+    real(8),dimension(3)                 :: coords
+    integer                              :: i, j, l, k, nbr_bins, kk, jj, ll
+    integer                              :: nbr_fibers
+    integer                              :: nbr_segments
+    integer                              :: orientation
+    !*********************************************
+    real(8), dimension(3)                :: ra, ra_end, rb, rb_end, pa, pb, Gab
+    real(8)                              :: Sab, Sba, la, lb, Gab_norm
+    logical                              :: flag, fibs_collide
+    real(8), dimension(:),  allocatable  :: bin_centers
+    real(8), dimension(:),  allocatable  :: bin_heights
+    real(8), dimension(:,:),allocatable  :: bin_limits
+    real(8)                              :: del_bin, z_orientation
+    !*********************************************
+    z_orientation=0.0
+    
+    namelist /input/ fiber_diameter,&         !Average fiber diameter
+    fiber_length,&           !Average fiber length
+    nbr_segments,&           !number of segments per fiber
+    volumetric_fraction,&    !Volumetric Fraction
+    max_theta,&              !Maximum fiber inclination
+    box_size,&               !Cluster Size
+    box_location,&             !Cluster location
+    z_orientation
 
-namelist /input/ fiber_diameter,&         !Average fiber diameter
-                 fiber_length,&           !Average fiber length
-                 nbr_segments,&           !number of segments per fiber
-                 volumetric_fraction,&    !Volumetric Fraction
-                 max_theta,&              !Maximum fiber inclination
-                 box_size,&               !Cluster Size
-                 box_location             !Cluster location
+    open(3,file='../INPUT/input.in', status='old')
 
-                         
-open(3,file='../INPUT/input.in', status='old')
+    open(30,file='../INPUT/initial_histogram.in', status='old')
 
-open(30,file='../INPUT/initial_histogram.in', status='old')
+    read(30,*),nbr_bins
+    print *,"hola", nbr_bins
+    allocate (bin_limits(nbr_bins,2))
+    allocate (bin_heights(nbr_bins))
+    print *,"hola22", nbr_bins
+    do i=1,nbr_bins
+        read(30,*), bin_heights(i)
+    end do
+    print *,"hola2222", nbr_bins
+    del_bin=pi/nbr_bins
 
-read(30,*),nbr_bins
-print *,"hola", nbr_bins
-allocate (bin_limits(nbr_bins,2))
-allocate (bin_heights(nbr_bins))
-print *,"hola22", nbr_bins
-do i=1,nbr_bins
-    read(30,*), bin_heights(i)
-end do
-print *,"hola2222", nbr_bins
-del_bin=pi/nbr_bins
+    do i=1,nbr_bins
+        bin_limits(i,1)=(i-1)*del_bin
+        bin_limits(i,2)=(i)  *del_bin
+    end do
+    print *,"hola33333", nbr_bins
+    read(3,nml = input)
+    print *,"hola44444", nbr_bins
+    close(3)
+    print *, "Box Location", box_location
+    print *, "Fiber Lenght", fiber_length
+    print *, "Fiber Diameter", fiber_diameter
 
-do i=1,nbr_bins
-    bin_limits(i,1)=(i-1)*del_bin
-    bin_limits(i,2)=(i)  *del_bin
-end do
-print *,"hola33333", nbr_bins
-read(3,nml = input)
-print *,"hola44444", nbr_bins
- close(3)
-print *, "Box Location", box_location
-print *, "Fiber Lenght", fiber_length
-print *, "Fiber Diameter", fiber_diameter
-
-!stop
-!*********************************************
-!*********************************************
-open(1, file="../OUTPUT/coords.txt")
-!open(2, file="../OUTPUT/coords_matlab.txt")
-
-  
-!print *, "FIB DIAM", fiber_diameter
-
-segment_length=fiber_length/nbr_segments
-print *,"Segment Length", segment_length
-
-if (segment_length<=(2.5d0*fiber_diameter)) then
-	print *, "Segment Length is too small when compared to the radius"
-	stop
-end if
-
-if (fiber_length>(0.95*box_size(1))) then
-	print *, "Fiber Length is close to or exceeds Box Size"
-	stop
-end if
-
-if (fiber_length>(0.95*box_size(2))) then
-	print *, "Fiber Length is close to or exceeds Box Size"
-	stop
-end if
-
-!if (fiber_length>(0.95*box_size(3))) then
-!	print *, "Fiber Length is close to or exceeds Box Size"
-!	stop
-!end if
-
-total_length_simulation=((box_size(1)*box_size(2)*box_size(3))*volumetric_fraction)/(pi*(fiber_diameter/2d0)**2d0)
-
-total_length=0
-
-nbr_fibers=floor(total_length_simulation/fiber_length)
-bin_heights=nint(nbr_fibers*bin_heights/sum(bin_heights))
-nbr_fibers=sum(bin_heights)
-
-do i=1, ubound(bin_heights,1)
-    print *, bin_heights(i)
-end do
+    !stop
+    !*********************************************
+    !*********************************************
+    open(1, file="../OUTPUT/coords.txt")
+    !open(2, file="../OUTPUT/coords_matlab.txt")
 
 
-print *, "nbr_fibers", nbr_fibers
+    !print *, "FIB DIAM", fiber_diameter
 
-write(1,*),nbr_fibers
-!write(2,*),nbr_fibers
-allocate(coords_ini(nbr_fibers,3))
-allocate(coords_end(nbr_fibers,3))
+    segment_length=fiber_length/nbr_segments
+    print *,"Segment Length", segment_length
+
+    if (segment_length<=(2.5d0*fiber_diameter)) then
+        print *, "Segment Length is too small when compared to the radius"
+        stop
+    end if
+
+    if (fiber_length>(0.95*box_size(1))) then
+        print *, "Fiber Length is close to or exceeds Box Size"
+        stop
+    end if
+
+    if (fiber_length>(0.95*box_size(2))) then
+        print *, "Fiber Length is close to or exceeds Box Size"
+        stop
+    end if
+
+    !if (fiber_length>(0.95*box_size(3))) then
+    !	print *, "Fiber Length is close to or exceeds Box Size"
+    !	stop
+    !end if
+
+    total_length_simulation=((box_size(1)*box_size(2)*box_size(3))*volumetric_fraction)/(pi*(fiber_diameter/2d0)**2d0)
+
+    total_length=0
+
+    nbr_fibers=floor(total_length_simulation/fiber_length)
+    bin_heights=nint(nbr_fibers*bin_heights/sum(bin_heights))
+    nbr_fibers=sum(bin_heights)
+
+    do i=1, ubound(bin_heights,1)
+        print *, bin_heights(i)
+    end do
 
 
-vol_frac=0
-total_length=0
+    print *, "nbr_fibers", nbr_fibers
 
-kk=1
+    write(1,*),nbr_fibers
+    !write(2,*),nbr_fibers
+    allocate(coords_ini(nbr_fibers,3))
+    allocate(coords_end(nbr_fibers,3))
 
-do i=1,nbr_bins
-    print *,"BIN",i
-    !print *,"KK",kk
-    do ll=1,bin_heights(i)
-        flag=.false.
-        do while (flag .eqv. .false.)
-	        flag=.true.
-	        do j=1,3
-		        call random_number(r)
-			    ini_pos(j)=box_location(j)+(box_size(j)/2)*(2*(r-0.5))
-	        end do
-	        !print *, flag 
-	        direction=1
-	
-	        call random_number(r)
-	        phi=pi/2+r*(bin_limits(i,2)-bin_limits(i,1))+bin_limits(i,1)
 
-            call random_number(r)
-	        theta=r*max_theta
-			        
-            vec_unit(1)=cos(theta)*cos(phi)
-            vec_unit(2)=cos(theta)*sin(phi)
-            vec_unit(3)=sin(theta)
-            !print *, "vec_Length", sqrt(dot_product(vec_unit, vec_unit))
-	        do j=1,3
-		        coords_ini(kk,j)=ini_pos(j)
-		        coords_end(kk,j)=ini_pos(j)+fiber_length*direction*vec_unit(j)
-            end do				
-	        do j=1, 3
- 		        if 	((coords_ini(kk,j) .gt. box_location(j)+box_size(j)/2).or.&
-	    	   	      coords_ini(kk,j) .lt. box_location(j)-box_size(j)/2)then
-			          flag=.false.
-		        end if
-	        end do
-            !print *,'ddd',flag
-	        do j=1, 3
- 		        if 	((coords_end(kk,j) .gt. box_location(j)+box_size(j)/2).or.&
-	    	   	      coords_end(kk,j) .lt. box_location(j)-box_size(j)/2)then
-			        flag=.false.
-		        end if
-	        end do
+    vol_frac=0
+    total_length=0
 
-	        do j=1,3
-		        rb(j)=coords_ini(kk,j)
-		        rb_end(j)=coords_end(kk,j)		 
-	        end do
-            !print *, "t",flag
-            if (kk.ne.1) then
-		        do k=1, kk-1
-			        do j=1,3
-				        ra(j)    =coords_ini(k,j)
-				        ra_end(j)=coords_end(k,j)	 
-			        end do
-			        fibs_collide=.false.
-			        call collide   (ra,&
-                 				    ra_end,&
-                 				    rb,&
-                 				    rb_end,&
-                 				    fiber_diameter,&
-                 				    fibs_collide)
-	
-			        if (fibs_collide.eqv. .true.) then
-				        flag=.false.
-			        end if                        
-           !print *,"ch4ttttttttttttttttttttttttttttttttttttttttt" 
-		        end do
-	        end if
+    kk=1
+
+    do i=1,nbr_bins
+        print *,"BIN",i
+        !print *,"KK",kk
+        do ll=1,bin_heights(i)
+            flag=.false.
+            do while (flag .eqv. .false.)
+                flag=.true.
+                ! Generate a random position
+                do j=1,3
+                    call random_number(r)
+                    ini_pos(j)=box_location(j)+(box_size(j)/2)*(2*(r-0.5))
+                end do
+                !print *, flag 
+                direction=1
+
+                call random_number(r)
+                phi=pi/2+r*(bin_limits(i,2)-bin_limits(i,1))+bin_limits(i,1)
+
+                call random_number(r)
+                theta=r*max_theta
+                !print *, "flag ", flag
+                ! To avoid fibers being aligned entirely in Z. In the precompression the will break.
+                if ( (abs(theta - 3.1416*0.5)) < z_orientation ) then
+                    flag=.false.
+                    CYCLE
+                end if
+                vec_unit(1)=cos(theta)*cos(phi)
+                vec_unit(2)=cos(theta)*sin(phi)
+                vec_unit(3)=sin(theta)
+                !print *, "vec_Length", sqrt(dot_product(vec_unit, vec_unit))
+                do j=1,3
+                    coords_ini(kk,j)=ini_pos(j)
+                    coords_end(kk,j)=ini_pos(j)+fiber_length*direction*vec_unit(j)
+                end do	
+                
+                !Check that fiber is within  the box
+                do j=1, 3
+                    if 	((coords_ini(kk,j) .gt. box_location(j)+box_size(j)/2).or.&
+                    coords_ini(kk,j) .lt. box_location(j)-box_size(j)/2)then
+                        flag=.false.
+                    end if
+                end do
+                ! if it is outside generate a new fiber
+                IF (.not. flag) then
+                    !print *, " ini is out"
+                    CYCLE
+                end if
+                
+                !print *,'ddd',flag
+                do j=1, 3
+                    if 	((coords_end(kk,j) .gt. box_location(j)+box_size(j)/2).or.&
+                    coords_end(kk,j) .lt. box_location(j)-box_size(j)/2)then
+                        flag=.false.
+                        
+                    end if      
+                end do
+                
+                
+                !print *, "coords ini ", coords_ini(kk,:)
+                !print *, "coords end ", coords_end(kk,:)       
+                ! print *, box_size/2.0
+                ! print *, "flag ", flag
+                ! if it is outside generate a new fiber
+                IF (.not. flag) then
+                    !print *, " end is out"
+                    CYCLE
+                end if
+                !print *, "Goes here "
+                
+                do j=1,3
+                    rb(j)=coords_ini(kk,j)
+                    rb_end(j)=coords_end(kk,j)		 
+                end do
+                !print *, "t",flag
+                
+                ! Check if it overlaps with the other fibers
+                if (kk.ne.1) then
+                    do k=1, kk-1
+                        fibs_collide=.false.
+                        
+                        do j=1,3
+                            ra(j)    =coords_ini(k,j)
+                            ra_end(j)=coords_end(k,j)	 
+                        end do
+                        
+                        ! Compare first their bounding boxes, It is a cheaper
+                        if (are_boxes_intersect(ra, ra_end,rb, rb_end, fiber_diameter)) then
+                        
+                        ! If their bounding boxes intersect find if the fibers overlap
+                        call collide2   (ra,&
+                        ra_end,&
+                        rb,&
+                        rb_end,&
+                        fiber_diameter,&
+                        fibs_collide)  
+                        end if
+
+
+                    if (fibs_collide.eqv. .true.) then
+                        ! this fiber is not good
+                        flag=.false. 
+                        ! generate a new fiber
+                        EXIT
+                    end if                        
+                    !print *,"ch4ttttttttttttttttttttttttttttttttttttttttt" 
+                end do
+            end if
         end do
-	
+
         do j=1,3
-	        coords(j)=coords_end(kk,j)-coords_ini(kk,j)
+            coords(j)=coords_end(kk,j)-coords_ini(kk,j)
         end do
         total_length=total_length+fiber_length
         !print *, "Number of Segments", nbr_segments
         write (1,*), nbr_segments+1
         write (2,*), nbr_segments+1
         do l=1,nbr_segments+1
-	        do j=1,3
-		        coords(j)=ini_pos(j)+(l-1)*segment_length*direction*vec_unit(j)
-	        end do
-	        write (1,*),0, real(coords(1),4), real(coords(2),4), real(coords(3),4)
-	        !write (2,*),   coords(1), coords(2), coords(3)
+            do j=1,3
+                coords(j)=ini_pos(j)+(l-1)*segment_length*direction*vec_unit(j)
+            end do
+            write (1,*),0, real(coords(1),4), real(coords(2),4), real(coords(3),4)
+            !write (2,*),   coords(1), coords(2), coords(3)
         end do
-    	
+
         vol_frac=(total_length*pi*(fiber_diameter/2)**2)/(box_size(1)*box_size(2)*box_size(3))
         print *,"Fiber", kk,"of",nbr_fibers,"   ","Volumetric Fraction",vol_frac
         kk=kk+1
-        
+
         if ((kk).gt. nbr_fibers)  stop
     end do
-end do
+    end do
 
-end program fiber_position
+    end program fiber_position
 
 
 
